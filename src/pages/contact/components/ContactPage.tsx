45a47b433155a45d31c16d1fcd7abf6651bbfcde
henrymunro <henrymunro73@gmail.com>
2019-03-17 09:27:31 +0000

    Make email clickable

--- src/pages/contact/components/ContactPage.tsx
+++ src/pages/contact/components/ContactPage.tsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import PageLayout from 'src/pages/components/PageLayout';
-import Video from 'src/pages/components/Video';
+import PageLayout from "src/pages/components/PageLayout";
+import Video from "src/pages/components/Video";
 
-import vid from 'src/assets/contact.mp4';
+import vid from "src/assets/contact.mp4";
 
-import styles from './ContactPage.module.scss';
+import styles from "./ContactPage.module.scss";
 
 const paragraph1 = `It would be great to connect with you!`;
 const paragraph2 = `Please email atweddell8@gmail.com`;
 
 type Props = {
-	inViewport: boolean;
+  inViewport: boolean;
 };
+
 export default class ContactPage extends React.PureComponent<Props> {
-	render() {
-		return (
-			<PageLayout
-				className={styles.page}
-				renderImages={() => <Video src={vid} className={styles.video} loop play={this.props.inViewport} />}
-				paragraphs={[ paragraph1, paragraph2 ]}
-				pageName="contact"
-			/>
-		);
-	}
+  render() {
+    return (
+      <a
+        className={styles.link}
+        href="mailto:atweddell8@gmail.com?Subject=Hello%20Alice"
+        target="_black"
+        rel="noopener"
+      >
+        <PageLayout
+          className={styles.page}
+          renderImages={() => (
+            <Video
+              src={vid}
+              className={styles.video}
+              loop
+              play={this.props.inViewport}
+            />
+          )}
+          paragraphs={[paragraph1, paragraph2]}
+          pageName="contact"
+        />
+      </a>
+    );
+  }
 }
