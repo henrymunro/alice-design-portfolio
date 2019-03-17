e5b7ab8a59a4b32b96a561ef6cd2b3a96621ef53
henrymunro <henrymunro73@gmail.com>
2019-03-17 09:27:21 +0000

    typo

--- src/pages/becoming-lost/components/BecomingLostPage.tsx
+++ src/pages/becoming-lost/components/BecomingLostPage.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import PageLayout from 'src/pages/components/PageLayout';
-import Video from 'src/pages/components/Video';
-import Image from 'src/pages/components/Image';
+import PageLayout from "src/pages/components/PageLayout";
+import Video from "src/pages/components/Video";
+import Image from "src/pages/components/Image";
 
-import cover from 'src/assets/becoming-lost/becoming-lost-cover.jpg';
-import img1 from 'src/assets/becoming-lost/becoming-lost-1.jpg';
-import img2 from 'src/assets/becoming-lost/becoming-lost-2.jpg';
-import img3 from 'src/assets/becoming-lost/becoming-lost-3.jpg';
-import img4 from 'src/assets/becoming-lost/becoming-lost-4.jpg';
-import vid from 'src/assets/becoming-lost/lost-vid.mp4';
+import cover from "src/assets/becoming-lost/becoming-lost-cover.jpg";
+import img1 from "src/assets/becoming-lost/becoming-lost-1.jpg";
+import img2 from "src/assets/becoming-lost/becoming-lost-2.jpg";
+import img3 from "src/assets/becoming-lost/becoming-lost-3.jpg";
+import img4 from "src/assets/becoming-lost/becoming-lost-4.jpg";
+import vid from "src/assets/becoming-lost/lost-vid.mp4";
 
-import styles from './BecomingLost.module.scss';
+import styles from "./BecomingLost.module.scss";
 
-const images = [ img1, img2, img3, img4 ];
+const images = [img1, img2, img3, img4];
 
 const paragraph1 = `The theme of this istd project was
 ‘lost’. the concept behind this japanese
@@ -27,35 +27,60 @@ a day to document a brief history of their
 relationship but also to give an insight
 into their marriage in their 80’s. soft
 typography has been used in this duotone
-book uses the serif font mrs eaves and
+book that uses the serif font mrs eaves and
 sans-serif mr eaves to differentiate male
 and female speakers.`;
 
 type Props = {
-	inViewport: boolean;
+  inViewport: boolean;
 };
 
 export default class BecomingLostPage extends React.PureComponent<Props> {
-	render() {
-		return (
-			<PageLayout
-				className={styles.page}
-				renderImages={this.renderImages}
-				paragraphs={[ paragraph1, paragraph2 ]}
-				pageName="becomingLost"
-			/>
-		);
-	}
+  render() {
+    return (
+      <PageLayout
+        className={styles.page}
+        renderImages={this.renderImages}
+        paragraphs={[paragraph1, paragraph2]}
+        pageName="becomingLost"
+      />
+    );
+  }
 
-	renderImages = () => {
-		return (
-			<div className={styles.wrapper}>
-				<Video src={vid} className={styles.largeImage} loop play={this.props.inViewport} />
-				<Image src={img1} alt="" className={styles.largeImage} images={images} />
-				<Image src={img2} alt="" className={styles.largeImage} images={images} />
-				<Image src={img4} alt="" className={styles.largeImage} images={images} />
-				<Image src={img3} alt="" className={styles.largeImage} images={images} />
-			</div>
-		);
-	};
+  renderImages = () => {
+    return (
+      <div className={styles.wrapper}>
+        <Video
+          src={vid}
+          className={styles.largeImage}
+          loop
+          play={this.props.inViewport}
+        />
+        <Image
+          src={img1}
+          alt=""
+          className={styles.largeImage}
+          images={images}
+        />
+        <Image
+          src={img2}
+          alt=""
+          className={styles.largeImage}
+          images={images}
+        />
+        <Image
+          src={img4}
+          alt=""
+          className={styles.largeImage}
+          images={images}
+        />
+        <Image
+          src={img3}
+          alt=""
+          className={styles.largeImage}
+          images={images}
+        />
+      </div>
+    );
+  };
 }
--- src/pages/walking-greenwich/components/WalkingGreenwichPage.tsx
+++ src/pages/walking-greenwich/components/WalkingGreenwichPage.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import PageLayout from 'src/pages/components/PageLayout';
-import Image from 'src/pages/components/Image';
+import PageLayout from "src/pages/components/PageLayout";
+import Image from "src/pages/components/Image";
 
-import greenwichLarge from 'src/assets/walking-greenwich/greenwich-large.jpg';
-import greenwichSmall from 'src/assets/walking-greenwich/greenwich-small.jpg';
+import greenwichLarge from "src/assets/walking-greenwich/greenwich-large.jpg";
+import greenwichSmall from "src/assets/walking-greenwich/greenwich-small.jpg";
 
-import styles from './WalkingGreenwich.module.scss';
+import styles from "./WalkingGreenwich.module.scss";
 
-const images = [ greenwichLarge, greenwichSmall ];
+const images = [greenwichLarge, greenwichSmall];
 
 const paragraph1 = `This data visualisation was a first year
 university project set to collect and
@@ -19,8 +19,8 @@ greenwich public to mark on a map the
 most beautiful place in greenwich and
 to describe how they felt in one word.
 plotting the locations meant i could get x
-and y coordiantes that i translated into
-cmyk vales. using this i created a walking
+and y coordinates that i translated into
+cmyk values. using this i created a walking
 route of the most beautiful places in
 greenwich as suggested by the public that
 could be followed in part or as a circular
@@ -28,22 +28,32 @@ route by typing into a gps device the
 coordinates.`;
 
 export default class WalkingGreenwichPage extends React.PureComponent {
-	render() {
-		return (
-			<PageLayout
-				renderImages={this.renderImages}
-				paragraphs={[ paragraph1, paragraph2 ]}
-				pageName="walkingGreenwich"
-			/>
-		);
-	}
+  render() {
+    return (
+      <PageLayout
+        renderImages={this.renderImages}
+        paragraphs={[paragraph1, paragraph2]}
+        pageName="walkingGreenwich"
+      />
+    );
+  }
 
-	renderImages() {
-		return (
-			<div>
-				<Image src={greenwichLarge} alt="Greenwich project" className={styles.topImage} images={images} />
-				<Image src={greenwichSmall} alt="Greenwich project" className={styles.topImage} images={images} />
-			</div>
-		);
-	}
+  renderImages() {
+    return (
+      <div>
+        <Image
+          src={greenwichLarge}
+          alt="Greenwich project"
+          className={styles.topImage}
+          images={images}
+        />
+        <Image
+          src={greenwichSmall}
+          alt="Greenwich project"
+          className={styles.topImage}
+          images={images}
+        />
+      </div>
+    );
+  }
 }
